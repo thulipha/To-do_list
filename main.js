660813d7c49cc88f951a72488f22a27c6eeba08e
thulikito501@gmail.com <thulikito501@gmail.com>
2023-07-28 05:08:21 -0300

    To do List v1

---
+++ main.js
@@ -0,0 +1,42 @@
+const $itemAdder = document.querySelector("[data-js='add-item']");
+const $sendButton = document.querySelector("[data-js='send-button']");
+const $list = document.querySelector("[data-js='list']");
+const $item = document.getElementsByTagName("li");
+
+
+
+
+$sendButton.addEventListener('click',addItem);
+
+
+function addItem(){
+  
+    let newItem = document.createElement('li');
+    
+    if($itemAdder.value){
+      
+      newItem.innerText = $itemAdder.value;
+      $itemAdder.value = '';
+  
+      let rmImage = document.createElement('img');
+      rmImage.srcset = 'assets/lixo.png';
+      
+      $list.appendChild(newItem);
+      newItem.appendChild(rmImage);
+      
+      function removeItem(){
+        newItem.remove();
+      };
+      
+      function doneItem(){
+        newItem.classList.toggle('done');
+      };
+      
+      newItem.addEventListener('dblclick',doneItem);
+      
+      return rmImage.addEventListener('click', removeItem);
+    };
+    
+    window.alert('O item não pode estar em branco, por favor insira um item válido!');
+};
+
